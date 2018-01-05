89037b79ca8bff96b93b574b89572d32e3ec23fa
Olena Ageyeva <eageeva@hotmail.com>
2018-01-05 02:58:24 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate :clickbait?
+
+  def clickbait
+    CLICKBAIT=["Won't Believe", "Secret", /Top [0..1]/, "Guess"]
+    flag=true
+    CLICKBAIT.each do |item|
+      if self.title.contain?(item) flag=false
+    end
+    return flag
+  end
 end
